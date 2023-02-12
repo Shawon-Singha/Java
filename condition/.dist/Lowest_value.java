c22d861be5400a77c24f579475510443d139ac71
Your Name <shawonsingha10@gmail.com>
2023-02-12 09:33:46 +0600

    update

---
+++ condition/.dist/Lowest_value.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Lowest_value 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int A,B,C;
+        System.out.println("Enter the value of A:");
+        A = sc.nextInt();
+
+        System.out.println("Enter the value of B:");
+        B = sc.nextInt();
+
+        System.out.println("Enter the value of C:");
+        C = sc.nextInt();
+
+        if((A<B) && (A<C))
+        {
+            System.out.println("A is the lowest value.");
+        }
+
+        else if((B<A) && (B<C))
+        {
+            System.out.println("B is the lowest value.");
+        }
+
+        else{
+            System.out.println("C is the lowest value.");
+        }
+      sc.close();  
+    }
+}
