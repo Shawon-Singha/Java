2878594bf3aeb2a2acd4a714cf65c137de3c4860
Your Name <shawonsingha10@gmail.com>
2023-02-19 12:52:15 +0600

    update

---
+++ Loop/Hollow_rhombus.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Hollow_rhombus 
+{
+    public static void main(String[] args) 
+    {
+         Scanner sc = new Scanner(System.in);
+
+         int rows;
+         System.out.print("Enter the number of rows:");
+
+         rows = sc.nextInt();
+
+         for(int rowno = 1; rowno<=rows; rowno++)
+         {
+            for(int spaceno = 1; spaceno<=rows - rowno; spaceno++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int colno = 1; colno<=rows; colno++)
+            {
+                if(colno == 1 || colno == rows || rowno == 1 || rowno == rows)
+                {
+                    System.out.print("*");
+                }
+
+                else 
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+         }
+     sc.close();
+    }
+}
+    
+    
+
+
