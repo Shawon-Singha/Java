9aa29a4b40f9857502aa9c57c54418801974067c
Your Name <shawonsingha10@gmail.com>
2023-03-03 22:26:05 +0600

    update

---
+++ W3resource/Exercise13.java
@@ -0,0 +1,13 @@
+public class Exercise13 
+{
+    static double Width = 5.5;
+    static double Height = 8.5;
+    public static void main(String[] args) 
+    {
+        double area = Width * Height;
+        double perimeter = 2 * (Width + Height);
+
+         System.out.printf("Area is %.1f",area);
+         System.out.printf("Perimeter is %.2d",perimeter);
+    }
+}
