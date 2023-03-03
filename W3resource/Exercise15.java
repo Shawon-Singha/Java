362a8736ebf61ae146e7f8038e6aa77b71d93b11
Your Name <shawonsingha10@gmail.com>
2023-03-03 22:43:32 +0600

    update

---
+++ W3resource/Exercise15.java
@@ -0,0 +1,15 @@
+public class Exercise15 
+{
+    public static void main(String[] args) 
+    {
+        int a=10,b = 20,temp;
+
+        System.out.println("Before swapping: a =  " +a +" b = "+ +b );
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:a = " +a +" b ="+ +b);
+    }
+}
