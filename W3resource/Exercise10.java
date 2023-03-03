179e7b51d5c7d9060e32dcf71972c1e1babe3a42
Your Name <shawonsingha10@gmail.com>
2023-03-03 21:00:49 +0600

    update

---
+++ W3resource/Exercise10.java
@@ -0,0 +1,9 @@
+public class Exercise10 
+{
+   public static void main(String[] args) 
+   {
+     double value = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+
+     System.out.println(value);
+   } 
+}
