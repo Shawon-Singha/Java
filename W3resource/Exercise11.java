909c0ae45ee50013555e3070c85a7f3da5b39f69
Your Name <shawonsingha10@gmail.com>
2023-03-03 21:30:21 +0600

    update

---
+++ W3resource/Exercise11.java
@@ -0,0 +1,12 @@
+public class Exercise11 
+{
+     static double radius = 7.5;
+    public static void main(String[] args) 
+    {
+        double perimeter = 2 *Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = "+perimeter);
+        System.out.println("Area is = "+area);
+    }
+}
