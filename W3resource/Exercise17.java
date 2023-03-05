7be358bb346b1bfa57fd3d326c1e7ed523176488
Your Name <shawonsingha10@gmail.com>
2023-03-05 22:48:02 +0600

    update

--- W3resource/Exercise17.java
+++ W3resource/Exercise17.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Exercise17 
 {
-    
+    public static void main(String[] args) 
+    {
+        int binary1,binary2;
+
+    }
 }
---
+++ W3resource/Exercise20.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Exercise20 
+{
+    public static void main(String[] args) 
+    {
+        Scanner ob1 = new Scanner(System.in);
+
+        System.out.println("Enter the decimal value :");
+        int value = ob1.nextInt();
+        
+        int []array = new int[20];
+
+        int i = 0;
+        
+
+        while(value!=0)
+        {
+            array[i]= value % 2;
+            value = value / 2;
+            i++;
+        }
+         
+        
+        for(int j = i-1; j>=0; j--)
+        {
+            System.out.print(array[j]);
+        }
+
+       
+
+        ob1.close();
+
+    }
+}
