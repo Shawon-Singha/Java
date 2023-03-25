6ce3dfc7c113a294e86ce5ca5330cd80f5cac107
Your Name <shawonsingha10@gmail.com>
2023-03-25 20:01:28 +0600

    update

---
+++ W3resource/Exercise43.java
@@ -0,0 +1,9 @@
+public class Exercise43 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Twinkle, twinkle, little star, \n\tHow I wonder what you are!");
+        System.out.println("\t\tUp above the world so high,\n\t\tLike a diamond in the sky.");
+        System.out.println("Twinkle, twinkle, little star, \n\tHow I wonder what you are");
+    }
+}
