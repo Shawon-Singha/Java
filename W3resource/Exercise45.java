30dcb81b4a85f11bc422093e8b21ca28bbfab6ef
Your Name <shawonsingha10@gmail.com>
2023-04-11 23:50:08 +0600

    update

---
+++ W3resource/Exercise45.java
@@ -0,0 +1,12 @@
+
+import java.io.File;
+
+public class Exercise45 {
+
+ public static void main(String[] args) {
+  System.out.println("/home/students/abc.txt  : " + new File("abc.txt").length() + " bytes");
+  System.out.println("/home/students/test.txt : " + new File("test.txt").length() + " bytes");
+ }
+}
+
+
