c7c97e2a54710a978bb460f85ee6cb5872e47c7b
Your Name <shawonsingha10@gmail.com>
2023-05-04 12:13:06 +0600

    update

---
+++ 4Array/Singha/ForEachLoop.java
@@ -0,0 +1,11 @@
+package Singha;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] num = { "Shawon", "Anik", "Tithe" };
+
+        for (String x : num) {
+            System.out.println(x);
+        }
+    }
+}
