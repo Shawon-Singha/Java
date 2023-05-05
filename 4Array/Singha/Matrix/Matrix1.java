898a3bbd7b51a7a6a8c004ef349c54bea937e74b
Your Name <shawonsingha10@gmail.com>
2023-05-05 12:29:21 +0600

    update

--- 4Array/Singha/Matrix/Matrix1.java
+++ 4Array/Singha/Matrix/Matrix1.java
@@ -4,62 +4,50 @@ import java.util.Scanner;
 
 public class Matrix1 {
     public static void main(String[] args) {
-        
+
         Scanner ob = new Scanner(System.in);
 
         int[][] a = new int[3][3];
-        int sum = 0, sum2 = 0, sum1 =0;
+        int sum = 0, sum2 = 0, sum1 = 0;
 
         System.out.println("Enter the matrix:");
-        for(int i =0; i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 a[i][j] = ob.nextInt();
             }
         }
 
-        //sum of diagonal elements.
-        for(int i =0; i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-               if(i == j)
-               {
-                sum = sum + a[i][j];
-               }
+        // sum of diagonal elements.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    sum = sum + a[i][j];
+                }
             }
         }
-        System.out.println("Sum od diaginal element: "+sum);
+        System.out.println("Sum od diaginal element: " + sum);
 
+        // sum of upper triangle.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i > j) {
 
-        //sum of upper triangle.
-        for(int i =0; i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-               if(i>j)
-               {
-                
-                sum1 = sum1 + a[i][j];
-               }
+                    sum1 = sum1 + a[i][j];
+                }
             }
         }
-        System.out.println("Sum of upper triangle: "+sum1);
+        System.out.println("Sum of upper triangle: " + sum1);
 
-        //sum of lower triangle.
-        for(int i =0; i<3;i++)
-        {
-            for(int j = 0;j<3;j++)
-            {
-               if(i<j)
-               {
-                 
-                sum2 = sum2 + a[i][j];
-               }
+        // sum of lower triangle.
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i < j) {
+
+                    sum2 = sum2 + a[i][j];
+                }
             }
         }
-        System.out.println("Sum of lower triangle:"+sum2);
+        System.out.println("Sum of lower triangle:" + sum2);
 
         ob.close();
     }
---
+++ 4Array/Singha/Matrix/Matrix2.java
@@ -0,0 +1,32 @@
+package Singha.Matrix;
+
+import java.util.Scanner;
+
+public class Matrix2 {
+    public static void main(String[] args) {
+        Scanner ob = new Scanner(System.in);
+
+        int[][] num = new int[4][5];
+        int k =0;
+
+        //Assing the value.
+        for(int i = 0;i<4;i++)
+        {
+            for(int j = 0;j<5;j++)
+            {
+                num[i][j] = k;
+                k++;
+            }
+        }
+
+        //printing the value.
+        for(int i = 0;i<4;i++)
+        {
+            for(int j = 0;j<5;j++)
+            {
+                System.out.print("\t"+num[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+}
