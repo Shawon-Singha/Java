e5e5c776bf3beaecf7d99757768731e6c5234177
Your Name <shawonsingha10@gmail.com>
2023-05-17 23:25:04 +0600

    update

---
+++ 4.1Arraylist/Singha/Main.java
@@ -0,0 +1,18 @@
+package Singha;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<>();  //creat arraylist
+
+        System.out.println("Size: "+number.size()); //to find the length of arrayList
+
+
+       /*
+        *  int a[] = new int[5];
+
+        System.out.println("Size of array: "+a.length);  // to find the length of arrayList
+        */
+    }
+}
