6ef22495f1cf6b095a78ef6c2558cd9f44b4842e
Your Name <shawonsingha10@gmail.com>
2023-05-25 15:19:34 +0600

    update

--- 5.1LinkedList/Anik/Munna.java
+++ 5.1LinkedList/Anik/Munna.java
@@ -3,13 +3,20 @@ package Anik;
 import java.util.LinkedList;
 
 public class Munna {
+   
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<String>();
 
         list.add("Shawon");
         list.add("Anik");
 
-         System.out.println(list +"\n");
+        System.out.println(list);
+
+        // Another way to add value
+        list.add( 2, "Hello");
+
+
+        //  System.out.println(list +"\n");
 
         // Another way for print
 
@@ -17,5 +24,20 @@ public class Munna {
         {
            System.out.print(name +" ");
         }
+
+
+        list.addFirst("Lion");  // before the list, add ....means it will be add in '0' index
+        list.addLast("Tiger");  // at last, this value will be added
+
+        System.out.println("\n"+list);
+
+        list.remove(4); // index '4' will be removed
+        System.out.println("\n"+ list);
+
+        list.remove("Lion");  // just "Lion " will be deleted
+        System.out.println(list);
+
+        list.removeAll(list);
+        System.out.println(list);
     }
 }
