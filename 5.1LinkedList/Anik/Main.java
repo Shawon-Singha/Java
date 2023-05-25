06c9834f106850718d713aa10eb86437c02b7ac4
Your Name <shawonsingha10@gmail.com>
2023-05-25 15:07:54 +0600

    update

---
+++ 5.1LinkedList/Anik/Main.java
@@ -0,0 +1,14 @@
+package Anik;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Shawon");
+        list.add("Singha");
+
+        System.out.println(list);
+    }
+}
